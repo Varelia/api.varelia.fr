358d3bd283c6bf34dbe85687f70b5604e80ba365
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-25 10:05:49 +0200

    fix: change config for prod env

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,10 @@ import path from 'path'
 
 export default {
     state: 'development',
-    cors: ['https://varelia.lucas-mercier.digital', 'https://varelia.fr', 'http://localhost:3000'],
+    cors: ['https://varelia.lucas-mercier.digital'],
     controllers: [path.resolve(__dirname, './api/presentation/controllers/**/*Controller.ts')],
     middlewares: [path.resolve(__dirname, './api/presentation/middlewares/**/*Middleware.ts')],
     app: {
-        port: 8080
+        port: 80
     }
 }
