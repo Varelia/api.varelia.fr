5a954d14e9a32c70018a415bd14703fab7aa535e
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-05-31 15:06:42 +0200

    add typeorm config

--- src/env.ts
+++ src/env.ts
@@ -12,5 +12,16 @@ export default {
     middlewares: [path.resolve(__dirname, './api/presentation/middlewares/**/*Middleware.ts')],
     app: {
         port: 8080
+    },
+    orm: {
+        driver: process.env.ORM_DRIVER || 'mysql',
+        host: process.env.ORM_HOST || 'localhost',
+        port: process.env.ORM_PORT || 3306,
+        db: {
+            username: process.env.ORM_DB_USERNAME || 'root',
+            password: process.env.ORM_DB_PASSWORD || '',
+            name: process.env.ORM_DB_NAME || 'varelia',
+            sync: process.env.ORM_DB_SYNC || false,
+        }
     }
 }
