61d3acf3caa4cbfc28ddf05c76f9d4e0d772f883
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-05-31 16:36:32 +0200

    create user entity

---
+++ src/api/domain/entities/User.ts
@@ -0,0 +1,34 @@
+import {BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity()
+export default class User {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column({length: 16})
+    nickname!: string
+
+    @Column()
+    password!: string
+
+    @Column({default: false})
+    admin!: boolean
+
+    @Column()
+    created_at!: Date
+
+    @Column()
+    updated_at!: Date
+
+    @BeforeInsert()
+    public addCreatedAdd() {
+        this.created_at = new Date()
+    }
+
+    @BeforeUpdate()
+    public modifyUpdatedAt() {
+        this.updated_at = new Date()
+    }
+
+}
