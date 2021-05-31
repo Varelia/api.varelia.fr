b9aba18b2b9246e19313beedda35e3da3ce8e0a2
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-05-31 16:38:20 +0200

    remove orm config

--- src/env.ts
+++ src/env.ts
@@ -14,19 +14,5 @@ export default {
     middlewares: [path.resolve(__dirname, './api/presentation/middlewares/**/*Middleware.ts')],
     app: {
         port: 8080
-    },
-    orm: {
-        driver: process.env.ORM_DRIVER || 'mysql',
-        host: process.env.ORM_HOST || 'localhost',
-        port: process.env.ORM_PORT || 3306,
-        entities: path.join(__dirname, process.env.ORM_ENTITIES as string),
-        seeds: path.join(__dirname, process.env.ORM_SEEDS as string),
-        factories: path.join(__dirname, process.env.ORM_SEEDS_FACTORIES as string),
-        db: {
-            username: process.env.ORM_DB_USERNAME || 'root',
-            password: process.env.ORM_DB_PASSWORD || '',
-            name: process.env.ORM_DB_NAME || 'varelia',
-            sync: process.env.ORM_DB_SYNC || false,
-        }
     }
 }
