aef04aec0f23d059579dbdde9792155d2c636a63
Lucas Mercier <lucas.mercier@viacesi.fr>
2021-06-18 10:56:26 +0200

    add seeds

--- src/api/domain/seeds/create-votes.seed.ts
+++ src/api/domain/seeds/create-votes.seed.ts
@@ -4,9 +4,11 @@ import {Vote} from '../entities'
 
 export default class CreateVotesSeed implements Seeder {
     async run(factory: Factory, connection: Connection): Promise<void> {
-        await factory(Vote)().createMany(50, {nickname: 'Lucas__Lks', address: '127.0.0.1'})
-        await factory(Vote)().createMany(25, {nickname: 'Lakorah', address: '127.0.0.1'})
-        await factory(Vote)().createMany(10, {nickname: 'Pixu_', address: '127.0.0.1'})
+        await factory(Vote)().createMany(500, {nickname: 'Lucas__Lks', address: '127.0.0.1'})
+        await factory(Vote)().createMany(250, {nickname: 'Lakorah', address: '127.0.0.1'})
+        await factory(Vote)().createMany(100, {nickname: 'Pixu_', address: '127.0.0.1'})
+        await factory(Vote)().createMany(50, {nickname: 'Zerqua_', address: '127.0.0.1'})
+        await factory(Vote)().createMany(25, {nickname: 'Saisaka', address: '127.0.0.1'})
     }
 
 }
